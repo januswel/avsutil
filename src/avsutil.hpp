c51ef00959e6e3735b5ad1972fc8360e0838c620
janus_wel <janus.wel.3@gmail.com>
2010-02-20 12:04:28 +0900

    Rewrite avsutil.hpp as interface header
    compiling fails still
    
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

--- src/avsutil.hpp
+++ src/avsutil.hpp
@@ -1,29 +1,66 @@
 /*
- * avsutil.h
- *  utility classes for AviSynth
+ * avsutil.hpp
+ *  Utility classes for AviSynth
  *  Copyright (C) 2010 janus_wel<janus.wel.3@gmail.com>
  *  see LICENSE for redistributing, modifying, and so on.
  *
- *      AudioInfo
- *      Avs
- *      FileWriter <- AudioWriter <- WavWriter
- *
- * refer
- *  RIFF WAV specifications
- *      http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/WAVE.html
- *
+ *      IAvs:           An interface class to handle avs file
+ *      IAudio:         An interface class to handle an audio stream in avs file
+ *      AudioInfo:      A struct that has informations about audio stream
+ *      AvsUtilError:   An exception class
  * */
 
-#ifndef AVSUTIL
-#define AVSUTIL
+#ifndef AVSUTIL_HPP
+#define AVSUTIL_HPP
 
 #include "avisynth.h"
-#include <string>
-#include <vector>
 #include <stdexcept>
-#include <stdio.h>
+#include <ostream>
 
 namespace avsutil {
+    class IAvs;
+    class IAudio;
+    struct AudioInfo;
+
+    // instantiate the object about avs
+    // the returned pointer MUST be "delete"ed
+    IAvs* CreateAvsObj(void);
+    IAvs* CreateAvsObj(const char* filename);   // with a filename
+
+    /*
+     * A class has basic features about avs,
+     * by wrapping classes defined in avisynth.h,
+     * e.g. opening avs file and getting audio informations.
+     * */
+    class IAvs {
+        public:
+            // to open a file
+            virtual void open(const char* avsfile) = 0;
+            // get informations
+            virtual const char* filename(void) = 0;
+            virtual bool is_fine(void) = 0;
+            virtual const char* errmsg(void) = 0;
+            // get audio
+            // the returned pointer MUST be "delete"ed
+            virtual IAudio* audio(void) = 0;
+
+            // typical destructor
+            virtual ~IAvs(void) {};
+    };
+
+    class IAudio {
+        public:
+            // get informations
+            virtual const AudioInfo& info(void) = 0;
+            // output audio data in the format of RIFF linear PCM
+            virtual void write(std::ostream&) const = 0;
+            // setter for the function to show progress
+            virtual void progress_callback(void (*progress_callback)(const unsigned __int64 processed_samples, const unsigned __int64 total_samples)) = 0;
+
+            // typical destructor
+            virtual ~IAudio(void) {};
+    };
+
     // informations of audio stream
     struct AudioInfo {
         bool exists;
@@ -34,126 +71,15 @@ namespace avsutil {
         unsigned __int16 block_size;    // channels * (bit_depth / 8)
     };
 
-    /*
-     * A class has basic features about avs,
-     * by wrapping classes defined in avisynth.h,
-     * e.g. opening avs file and getting audio informations.
-     * */
-    class Avs {
-        protected:
-            IScriptEnvironment* se;
-            PClip clip;
-            VideoInfo vi;
-            AudioInfo ai;
-
-            void pack_audio_info() {
-                ai.exists        = vi.HasAudio();
-                ai.samples       = vi.num_audio_samples;
-                ai.sampling_rate = vi.SamplesPerSecond();
-                ai.channels      = vi.AudioChannels();
-                ai.bit_depth     = get_bit_depth();
-                ai.block_size    = vi.BytesPerAudioSample();
-            };
-
-            unsigned int get_bit_depth() const {
-                switch (vi.sample_type) {
-                    case SAMPLE_INT8:
-                        return 8;
-                    case SAMPLE_INT16:
-                        return 16;
-                    case SAMPLE_INT24:
-                        return 24;
-                    case SAMPLE_INT32:
-                        return 32;
-                    case SAMPLE_FLOAT:
-                        return sizeof(float) * 8;
-                    default:
-                        throw std::range_error("unknown sample type");
-                }
-            };
-
-        public:
-            Avs() {};
-            Avs(const std::string& avsfile) { open(avsfile); };
-            virtual ~Avs() {};
-            void open(const std::string& avsfile);
-            const AudioInfo& get_audioinfo() const { return ai; };
-            void get_audio(void* buf, const __int64 start, const __int64 count) { clip->GetAudio(buf, start, count, se); };
-    };
-
-    /*
-     * An ancestor class for writing file.
-     * This is an abstract class.
-     * */
-    class FileWriter {
-        public:
-            virtual bool write(const std::string& wavfile, void (*progress)(const unsigned __int64, const unsigned __int64)) = 0;
-    };
-
-    /*
-     * A super class for writing audio file.
-     * This is abstract class.
-     * In order to make the class to write audio file in your favorite format,
-     * implement write_header(), write_footer() and write_data(),
-     * in your sub classes for this.
-     * */
-    class AudioWriter : public FileWriter {
-        protected:
-            FILE* audiofp;    // file pointer for the file
-            Avs avs;
-
-            virtual void write_header() = 0;
-            virtual void write_footer() = 0;
-            virtual void write_data(void (*progress)(const unsigned __int64, const unsigned __int64)) = 0;
-
-        public:
-            AudioWriter() {};
-            AudioWriter(const std::string& avsfile) : avs(avsfile) {};
-            virtual ~AudioWriter() {};
-
-            const Avs& get_avs() const { return avs; };
-            void open(const std::string& avsfile) { avs.open(avsfile); };
-            virtual bool write(const std::string& wavfile, void (*progress)(const unsigned __int64, const unsigned __int64) = NULL);
-    };
-
-    class WavWriter : public AudioWriter {
-        private:
-            // constants
-            static const unsigned __int16 header_size;
-            static const unsigned __int16 wave_header_size;
-            static const unsigned __int16 fmt_chunk_size;
-            static const char* header_riff;
-            static const char* header_wave;
-            static const char* header_fmt;
-            static const char* header_data;
-            enum {PCM = 1};     // only linear PCM format for now
-
-            // default of buf_samples
-            //  actual buffer size is:
-            //      min     buf_samples *  1 byte ( 8bit, mono)
-            //      max     buf_samples * 24 byte (32bit, 5.1ch)
-            static const unsigned __int32 buf_samples_def = 0x1000;
-
-            // a number of samples that processed at one time
-            unsigned int buf_samples;
-
-        protected:
-            virtual void write_header();
-            virtual void write_footer() {};
-            virtual void write_data(void (*progress)(const unsigned __int64, const unsigned __int64));
-
-        public:
-            WavWriter(const unsigned int n = buf_samples_def)
-                : buf_samples(n) {};
-            WavWriter(const std::string& avsfile, const unsigned int n = buf_samples_def)
-                : buf_samples(n), AudioWriter(avsfile) {};
-            virtual ~WavWriter() {};
-    };
-
     class AvsUtilError : public std::domain_error {
         public:
-            AvsUtilError(const std::string& errmsg) : std::domain_error(errmsg) {};
+            AvsUtilError(const char* errmsg) : std::domain_error(errmsg) {};
     };
 };
 
-#endif
+// output audio data in the format of RIFF linear PCM
+std::ostream& operator <<(std::ostream&, const avsutil::IAudio&);
+std::ostream& operator <<(std::ostream&, const avsutil::IAudio* const);
+
+
+#endif // AVSUTIL_HPP
