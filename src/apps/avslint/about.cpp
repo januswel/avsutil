0a0445c140191f1caa85c130784ca898f806a32b
janus_wel <janus.wel.3@gmail.com>
2010-05-25 17:43:33 +0900

    Fix preceding comments
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

--- src/apps/avslint/about.cpp
+++ src/apps/avslint/about.cpp
@@ -2,8 +2,8 @@
  * about.cpp
  *  A definition of function to give informations "about" the program
  *
- *  written by janus_wel<janus.wel.3@gmail.com>
- *  This source code is in public domain, and has NO WARRANTY.
+ *  Copyright (C) 2010 janus_wel<janus.wel.3@gmail.com>
+ *  see LICENSE for redistributing, modifying, and so on.
  * */
 
 #include "avslint.hpp"
