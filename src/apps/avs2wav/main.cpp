143438cefe91930970954c07abe7f5d43d7c4b6e
janus_wel <janus.wel.3@gmail.com>
2010-06-09 19:17:11 +0900

    Remove redundant try-catch clause
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

--- src/apps/avs2wav/main.cpp
+++ src/apps/avs2wav/main.cpp
@@ -49,88 +49,82 @@ int Main::main(void) {
         throw avs2wav_error(BAD_ARGUMENT, "Specify <inputfile>\n");
     }
 
-    try {
-        // read in avs file
-        avs_type& avs = manager().load(inputfile.c_str());
-        if (!avs.is_fine()) {
-            throw avs2wav_error(BAD_AVS, avs.errmsg());
-        }
-
-        // get audio stream
-        audio_type& audio = avs.audio();
-        audio_type::info_type ai = audio.info();
-
-        if (!ai.exists) {
-            throw avs2wav_error(BAD_AVS,
-                    "Specified file has no audio stream: " + inputfile);
-        }
-
-        // preparations
-        // output and information stream (to stdout for now)
-        ostream outputs(cout.rdbuf());
-        ostream infos(cout.rdbuf());
-        // apply manipulators to form data
-        progresss << right << fixed << setprecision(2);
-        infos << left;
-
-        // settings for infos, outputs and progresss
-        // this is true if redirected to file or connected to pipe
-        if (!util::io::is_redirected()) {
-            // if output to file
-
-            // set output filename if it isn't decided still
-            if (outputfile.empty()) outputfile.assign(inputfile).append(".wav");
-
-            // create filebuf and set it output stream
-            filebuf* fbuf = new filebuf;
-            fbuf->open(outputfile.c_str(), ios::out | ios::binary | ios::trunc);
-            if (!fbuf->is_open()) {
-                cerr << "Can't open file to write: " << outputfile << endl;
-                return UNKNOWN;
-            }
-
-            // set filebuf to output stream
-            outputs.rdbuf(fbuf);
-        }
-        else {
-            // if output to stdout
-
-            // only to show
-            outputfile.assign("stdout");
-            // use stderr to show a progress of the process and informations of avs
-            progresss.rdbuf(cerr.rdbuf());
-            infos.rdbuf(cerr.rdbuf());
-            // set stdout to binary mode (Windows only)
-            util::io::set_stdout_binary();
-        }
-
-        infos
-            << setw(header_width) << "source:"                << inputfile << endl
-            << setw(header_width) << "destination:"           << outputfile << endl
-            << setw(header_width) << "buffers for output:"    << buf_size << " bytes" << endl
-            << setw(header_width) << "processed at one time:" << buf_samples << " samples" << endl
-            << ai;
-
-        // allocate buffer
-        std::vector<char> internalbuf(buf_size);
-        outputs.rdbuf()->pubsetbuf(&internalbuf[0], buf_size);
-        // set values to audio_type
-        audio.buf_samples(buf_samples);
-        audio.progress_callback(progress_cl);
-
-        // do it
-        outputs << audio;
-
-        infos
-            << endl
-            << endl
-            << "done." << endl
-            << endl;
+    // read in avs file
+    avs_type& avs = manager().load(inputfile.c_str());
+    if (!avs.is_fine()) {
+        throw avs2wav_error(BAD_AVS, avs.errmsg());
     }
-    catch (exception& ex) {
-        cerr << endl << ex.what() << endl;
-        return UNKNOWN;
+
+    // get audio stream
+    audio_type& audio = avs.audio();
+    audio_type::info_type ai = audio.info();
+
+    if (!ai.exists) {
+        throw avs2wav_error(BAD_AVS,
+                "Specified file has no audio stream: " + inputfile);
     }
+
+    // preparations
+    // output and information stream (to stdout for now)
+    ostream outputs(cout.rdbuf());
+    ostream infos(cout.rdbuf());
+    // apply manipulators to form data
+    progresss << right << fixed << setprecision(2);
+    infos << left;
+
+    // settings for infos, outputs and progresss
+    // this is true if redirected to file or connected to pipe
+    if (!util::io::is_redirected()) {
+        // if output to file
+
+        // set output filename if it isn't decided still
+        if (outputfile.empty()) outputfile.assign(inputfile).append(".wav");
+
+        // create filebuf and set it output stream
+        filebuf* fbuf = new filebuf;
+        fbuf->open(outputfile.c_str(), ios::out | ios::binary | ios::trunc);
+        if (!fbuf->is_open()) {
+            cerr << "Can't open file to write: " << outputfile << endl;
+            return UNKNOWN;
+        }
+
+        // set filebuf to output stream
+        outputs.rdbuf(fbuf);
+    }
+    else {
+        // if output to stdout
+
+        // only to show
+        outputfile.assign("stdout");
+        // use stderr to show a progress of the process and informations of avs
+        progresss.rdbuf(cerr.rdbuf());
+        infos.rdbuf(cerr.rdbuf());
+        // set stdout to binary mode (Windows only)
+        util::io::set_stdout_binary();
+    }
+
+    infos
+        << setw(header_width) << "source:"                << inputfile << endl
+        << setw(header_width) << "destination:"           << outputfile << endl
+        << setw(header_width) << "buffers for output:"    << buf_size << " bytes" << endl
+        << setw(header_width) << "processed at one time:" << buf_samples << " samples" << endl
+        << ai;
+
+    // allocate buffer
+    std::vector<char> internalbuf(buf_size);
+    outputs.rdbuf()->pubsetbuf(&internalbuf[0], buf_size);
+    // set values to audio_type
+    audio.buf_samples(buf_samples);
+    audio.progress_callback(progress_cl);
+
+    // do it
+    outputs << audio;
+
+    infos
+        << endl
+        << endl
+        << "done." << endl
+        << endl;
 
     return OK;
 }
