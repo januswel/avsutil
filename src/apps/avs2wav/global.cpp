a1f2ac489b915f0ad3326f4d378b85409ba73224
janus_wel <janus.wel.3@gmail.com>
2010-06-18 14:35:21 +0900

    Separate declarations of global objects from main.cpp
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

---
+++ src/apps/avs2wav/global.cpp
@@ -0,0 +1,14 @@
+/*
+ * global.cpp
+ *  Declarations for global objects
+ *
+ *  written by janus_wel<janus.wel.3@gmail.com>
+ *  This source code is in public domain, and has NO WARRANTY.
+ * */
+
+#include "avs2wav.hpp"
+#include <locale>
+
+util::string::typeconverter tconv(std::locale::classic());
+util::string::check checker(std::locale::classic());
+
