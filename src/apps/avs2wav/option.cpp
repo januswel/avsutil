6b1f47e6d30fa00f9858d36b146ab6f0ee3c8b69
janus_wel <janus.wel.3@gmail.com>
2010-06-20 22:59:33 +0900

    Add a definition of a static const var
    In ANSI C++, the definition of static const variables are required,
    contrast to they are needless with a VC++ language extension.
    
    Signed-off-by: janus_wel <janus.wel.3@gmail.com>

---
+++ src/apps/avs2wav/option.cpp
@@ -0,0 +1,12 @@
+/*
+ * option.cpp
+ *  Definitions for option.hpp
+ *
+ *  Copyright (C) 2010 janus_wel<janus.wel.3@gmail.com>
+ *  see LICENSE for redistributing, modifying, and so on.
+ * */
+
+#include "option.hpp"
+
+const unsigned int opt_buffers_type::buf_size_min;
+
